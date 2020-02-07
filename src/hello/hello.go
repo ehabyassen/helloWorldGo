4416a23fcf6454c5162251db58c43f0093235eeb
Ehab Abdelaziz <ehab.abdelaziz@episerver.com>
2020-02-07 10:33:51 +0100

    define a new package and use it in the main package

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ehabyassen/hello/morestrings"
+)
 
 func main() {
 	fmt.Printf("Hello, world!\nI am Ehab.\n")
+	fmt.Printf(morestrings.ReverseRunes("\n!oG ,olleH"))
 }
